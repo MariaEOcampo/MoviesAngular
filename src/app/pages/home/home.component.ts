5bbba64914390fad907a8b7c98ce7e35a82a76d4
emilia.ocampo <emilia.ocampo@proveedor.ues21.edu.ar>
2021-08-21 13:07:53 -0300

    last changes on styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from '@auth0/auth0-angular';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass'],
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   constructor(public auth: AuthService) {}
